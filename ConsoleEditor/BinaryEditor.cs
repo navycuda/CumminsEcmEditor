3630053e533013ff8b6c17f0db476fb33f01a1bc
NavyCuda <navysupra@gmail.com>
2022-09-18 23:06:00 -0700

    Break program into seperate classes

---
+++ ConsoleEditor/BinaryEditor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ConsoleEditor
+{
+  public static class BinaryEditor
+  {
+    public static void Run(string[] args)
+    {
+      Console.WriteLine("Now in BinaryEditor\n");
+
+      string loadBinPath = args[0];
+      string saveXcalPath = args[1];
+
+      Console.WriteLine($"loading from : {loadBinPath}");
+      Console.WriteLine($"saving to    : {saveXcalPath}");
+
+
+
+
+
+    }
+  }
+}
+
